306ea9451243b1b50706eac162a1a640434dba31
malthewejnold <malthewejnold@gmail.com>
2023-10-24 10:25:11 +0200

    Player in the game

--- main.gd
+++ main.gd
@@ -1,2 +1,11 @@
 extends Node2D
 
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
